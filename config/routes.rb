b6ce8953941962cb03e46d9ba2cf79579694b95d
EC2 Default User <ec2-user@ip-172-31-13-239.ec2.internal>
2021-04-04 07:13:23 +0000

    [Finish]2-5

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'todolists/new'
   get 'top' => 'homes#top'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  
 end
